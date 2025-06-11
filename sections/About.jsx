3f15fd672b7e42247656f5b7c391517e535a8a3f
DarrylCD <daredarryl@rediffmail.com>
2025-06-11 12:16:46 +0530

    feature : add typing text component

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
-import { TypingText } from '../components';
+import { TypingText } from '../components/CustomTexts.jsx';
 import { slideIn, staggerContainer } from '../utils/motion.js';
 import styles from '../styles';
 
