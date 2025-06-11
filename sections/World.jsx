c0637504ce44b5f645b80dd21ff3fa5ce0d33857
DarrylCD <daredarryl@rediffmail.com>
2025-06-11 17:10:57 +0530

    feature : add World section implementation

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,25 @@
 'use client';
+import { motion } from 'framer-motion';
+import { TypingText, TitleText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion.js';
+import styles from '../styles';
+import { newFeatures } from '../constants';
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`} id="get-started">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People in the world" textStyles="text-center" />
+      <TitleText
+        title={<>Track friends around you and invite them to play together in the same world</>}
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
