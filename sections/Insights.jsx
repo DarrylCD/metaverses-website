3e3044c0094dd1bb4e3f09aef0eaf1dd1a45fed9
DarrylCD <daredarryl@rediffmail.com>
2025-06-11 17:39:31 +0530

    feature : add initial setup for Insights section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,37 @@
 'use client';
+import { motion } from 'framer-motion';
+import { TypingText, InsightCard, TitleText } from '../components';
+import { staggerContainer } from '../utils/motion.js';
+import styles from '../styles';
+import { insights } from '../constants';
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`} id="get-started">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insights" textStyles="text-center" />
+      <TitleText
+        title={<>Insight about metaverses</>}
+        textStyles="text-center"
+      />
+      <motion.div
+        variants={staggerContainer}
+        className="mt-[50px] flex flex-wrap justify-center gap-[24px]"
+      >
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`} // Ensure unique key for each card
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </motion.div>
+    </motion.div>
   </section>
 );
 
