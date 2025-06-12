4fa11adaea4e343653e5530bcd468b2eb711f79e
DarrylCD <daredarryl@rediffmail.com>
2025-06-12 17:01:51 +0530

    configure : add && next export to export the satic files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true, //Set appDir: false if your project is 
+    // based on the pages directory and you want to avoid 
+    // any issues related to the new App Router during deployment.
   },
 };
 
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+const Page = () => (
+  <div className='bg-primary-black overflow-hidden'>
+    <Navbar />
+    <Hero />
+    <div className='relative'>
+      <About />
+      <div className='gradient-03 z-0' />
+      <Explore />
+    </div>
+    <div className='relative'>
+      <GetStarted />
+      <div className='gradient-04 z-0' />
+      <WhatsNew />
+    </div>
+    <World />
+    <div className='relative'>
+      <Insights />
+      <div className='gradient-05 z-0' />
+      <Feedback />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Page;
+// This code is the main entry point for a Next.js application that renders a series of sections
+// including a navigation bar, hero section, and various content sections like About, Explore, Get Started, Whats New, World, Insights, Feedback, and Footer.
